c0e4c60320e057055d95e093cee895243415a97a
Jake-Howell <jakehowell8@gmail.com>
2024-03-11 16:42:18 +0000

    added country_code to cmdline.txt

--- BASH Scripts/setup_ap.sh
+++ BASH Scripts/setup_ap.sh
@@ -14,7 +14,7 @@ echo "Installing required packages..."
 sudo apt install -y hostapd dnsmasq iptables
 
 # Stop wpa supplicant
-systemctl stop wpa_supplicant
+# systemctl stop wpa_supplicant
 
 #Following 2 commands @ 1:31 of yt video to be put into dnsmasq.conf
 ####################################################################
@@ -25,6 +25,11 @@ interface=wlan0
 
 # Set IP Range for Clients
 dhcp-range=192.168.4.2,192.168.4.20,255.255.255.0,24h
+EOF
+
+# Set wlan country code
+cat <<EOF | sudo tee /boot/firmware/cmdline.txt
+ cfg80211.ieee80211_regdom=GB
 EOF
 
 # hostapd.conf
@@ -44,7 +49,7 @@ channel=1
 # Set Deiver
 driver=nl80211
 
-# Set country code
+# Set country code for AP
 country_code=GB
 EOF
 
